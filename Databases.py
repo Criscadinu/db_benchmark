c9497d975749cef1dbbf60a02f59aaa42015d1c3
danny schot <d.schot@absturzsicherung.de>
2020-04-26 15:02:26 +0200

    edit HBase

--- Databases.py
+++ Databases.py
@@ -146,3 +146,21 @@ class Redis(Database):
         }
         self.connection.hmset("uitvoering"+str(id), uitvoering)
         return
+
+class Happybase(Database):
+    def __init__(self):
+        self.backlog_count_ = 0
+        self.backlog_upper_bound_ = 20
+
+    def connection(self):
+        conn = happybase.Connection(host = host)
+        conn.open()
+        return conn
+
+    def write(self, drone_update):
+        self.table = connection.table('PARIS')
+        self.batch = table.batch()
+        self.batch.put(datetime.datetime, {'drone:id':  drone_update.drone_id, 'drone_lat': drone_update.drone_lat, 'drone_long': drone_update.drone_long,'batterij_duur': drone_update.batterij_duur})
+        self.batch.delete(b'row-key-4')
+        self.batch.send()
+        return
--- HBase.py
+++ HBase.py
@@ -1,40 +1,24 @@
 import happybase
 from datetime import datetime
 from Databases import Database
-from DroneData import *
 
 
 
 class Happybase(Database):
-    def __init__(self, setting):
-        self.
+    def __init__(self):
         self.backlog_count_ = 0
         self.backlog_upper_bound_ = 20
-        self.table_ = None
-        self.batch_ = None
-
-        self.batch_size_ = int(setting.get('batch_size'))
-
-        self.connection = None
 
 def connection(self):
-    conn = happybase.Connection(host = host,
-                                table_prefix = namespace,
-                                table_prefix_separator = ":")
+    conn = happybase.Connection(host = host)
     conn.open()
-    table = conn.table(table_name)
-    batch = table.batch(batch_size = batch_size)
-    return conn, batch
+    return conn
 
-
-    uitvoering = {
-        "tijd": datetime.datetime.utcnow(),
-        "drone_id": "drone" + str(drone_update.drone_id),
-        "drone_lat": drone_update.drone_lat,
-        "drone_long": drone_update.drone_long,
-        "batterij_duur": drone_update.batterij_duur
-    }
 def write(self, drone_update):
-    print('Writing ...')
-    put 'PARIS', datetime.datetime, 'drone:id',  + str(drone_update.drone_id), 'drone_lat': drone_update.drone_lat,'drone_long': drone_update.drone_long, 'batterij_duur': drone_update.batterij_duur
+    print(self.conn.tables())
+    self.table = connection.table('PARIS')
+    self.batch = table.batch()
+    self.batch.put(datetime.datetime, {'drone:id':  drone_update.drone_id, 'drone_lat': drone_update.drone_lat, 'drone_long': drone_update.drone_long,'batterij_duur': drone_update.batterij_duur})
+    self.batch.delete(b'row-key-4')
+    self.batch.send()
     return
